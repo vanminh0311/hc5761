5a2facc054958346f63304489871180520804edf
vanminh0311 <85997835+vanminh0311@users.noreply.github.com>
2022-02-08 09:12:25 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,22 @@
 
 # Add a feed source
 git clone https://github.com/aliass532/luci-app-eqos package/eqos
+
+##src-git packages https://git.lede-project.org/feed/packages.git
+#src-git luci https://git.lede-project.org/project/luci.git
+##src-git routing https://git.lede-project.org/feed/routing.git
+##src-git telephony https://git.lede-project.org/feed/telephony.git
+#src-git video https://github.com/openwrt/video.git
+#src-git targets https://github.com/openwrt/targets.git
+#src-git management https://github.com/openwrt-management/packages.git
+#src-git oldpackages http://git.openwrt.org/packages.git
+#src-link custom /usr/src/openwrt/custom-feed
+#src-git luci https://github.com/wifimedia/luci-17.git;gateway
+#src-git wifimedia https://github.com/wifimedia/lede_wifimedia.git;wr841v13
+#src-git wifimedia https://github.com/wifimedia/lede_wifimedia.git;wr840v4
+#src-git wifimedia https://github.com/wifimedia/lede_wifimedia.git;wr940v5
+git wifimedia https://github.com/wifimedia/lede_wifimedia.git;wifimedia_ex
+git luci https://github.com/wifimedia/luci-17.git;WRT
+
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
